6b8a4a63dc468cbe1d315e41fdf0b869f603a99c
Sleny.Zl <1402271195@qq.com>
2020-02-24 20:58:08 +0800

    Update add_timing_data.py

--- cgi-bin/add_timing_data.py
+++ cgi-bin/add_timing_data.py
@@ -1,19 +1,34 @@
 #! /usr/local/bin/python3
 
-import os
+# import os
 import cgi
-import time
-import sys
+# import time
+# import sys
 import yate
+import sqlite3
 
 print(yate.start_response('text/plain'))
-addr = os.environ['REMOTE_ADDR']
-host = os.environ['REMOTE_HOST']
-method = os.environ['REQUEST_METHOD']
-cur_time = time.asctime(time.localtime())
-print(host + ", " + addr + ", " + cur_time + ": " + method + ": ", end=' ', file=sys.stderr)
+
 form = cgi.FieldStorage()
-for each_form_item in form.keys():
-    print(each_form_item + "->" + form[each_form_item].value, end='', file=sys.stderr)
-print(file=sys.stderr)
+the_id = form_data['Athlete'].value
+the_time = form_data['Time'].value
+
+connection = sqlite3.connect('coachdata.sqlite')
+cursor = sqlite3.Cursor()
+cursor.execute("INSERT INTO timing_data (athlete_id, value) VALUES (?, ?)", (the_id, the_time))
+connection.commit()
+connection.close()
+
 print("OK")
+
+# print(yate.start_response('text/plain'))
+# addr = os.environ['REMOTE_ADDR']
+# host = os.environ['REMOTE_HOST']
+# method = os.environ['REQUEST_METHOD']
+# cur_time = time.asctime(time.localtime())
+# print(host + ", " + addr + ", " + cur_time + ": " + method + ": ", end=' ', file=sys.stderr)
+# form = cgi.FieldStorage()
+# for each_form_item in form.keys():
+#     print(each_form_item + "->" + form[each_form_item].value, end='', file=sys.stderr)
+# print(file=sys.stderr)
+# print("OK")
